de09f56dcb975b4b55de66c32f009ec3ff03f20f
AodenTeo <aodenteo@gmail.com>
2024-11-10 19:48:12 -0800

    Moved the project files to correct place

---
+++ scratch.py
@@ -0,0 +1,25 @@
+import torch
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+import math
+import torch.nn as nn
+import numpy as np
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Process the dataset from the file art.csv
+class ArtDataset(Dataset):
+    def __init__(self):
+        # data loading
+        xy = pd.read_csv('art.csv')
+        print(xy)
+
+    def __getitem__(self, index):
+        return index
+
+    def __len__(self):
+        return 1
+dataset = ArtDataset()
